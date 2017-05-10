7e5521663dcd90c0cc9d636ef602bf462642bb81
hoangcongst <hoangcongst@gmail.com>
2017-05-10 23:03:01 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,59 +1,124 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Home</div>
+    @if (Auth::check())
 
-                <div class="panel-body">
-                    Trang chu
-                    <p>Danh sach tat cac bai post</p>
-                    <table class="table table-striped m-b-none">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Action</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($posts as $post)
-                            <tr>
-                                <td>
-                                    <a href="{{ URL::to('post_detail/'.$post->id) }}">{!!html_entity_decode($post->title)!!}</a>
-                                    </td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                    <br>
-                    <p>Danh sach cac chu de</p>
-                    <table class="table table-striped m-b-none">
-                        <thead>
-                        <tr>
-                            <th>Name</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach ($categories as $category)
-                            <tr>
-                                @if (Auth::check())
-                                <td>
-                                    <a href="{{ URL::to('adminpost/'.$category->id) }}">{!!html_entity_decode($category->name)!!}</a>
-                                </td>
-                                @else
-                                <td>
-                                    <a href="{{ URL::to('post/'.$category->id) }}">{!!html_entity_decode($category->name)!!}</a>
-                                </td>
-                                @endif
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Home</div>
+
+                        <div class="panel-body">
+                            Trang chu
+                            <p>Danh sach tat cac bai post</p>
+                            <table class="table table-striped m-b-none">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Action</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($posts as $post)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ URL::to('post_detail/'.$post->id) }}">{!!html_entity_decode($post->title)!!}</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                            <br>
+                            <p>Danh sach cac chu de</p>
+                            <table class="table table-striped m-b-none">
+                                <thead>
+                                <tr>
+                                    <th>Name</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ($categories as $category)
+                                    <tr>
+                                        <td>
+                                            <a href="{{ URL::to('adminpost/'.$category->id) }}">{!!html_entity_decode($category->name)!!}</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
+    @else
+        <!-- content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-9">
+                    <div class="blog-post">
+                        @foreach ($posts as $post)
+                            <div class="panel">
+                                <div>
+                                    <img src="{{ $post->avatar }}" class="img-full">
+                                </div>
+                                <div class="wrapper-lg">
+                                    <h2 class="m-t-none"><a href="{{ URL::to('post_detail/'.$post->id) }}">{!!html_entity_decode($post->title)!!}</a>
+                                    </h2>
+                                    <div>
+                                        {!!html_entity_decode($post->description)!!}
+                                    </div>
+                                    <div class="line line-lg b-b b-light"></div>
+                                    <div class="text-muted">
+                                        <i class="fa fa-user text-muted"></i> by <a href class="m-r-sm">Admin</a>
+                                        <i class="fa fa-clock-o text-muted"></i> {{ $post->updated_at }}
+                                        <a href class="m-l-sm"><i class="fa fa-comment-o text-muted"></i> 2 comments</a>
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <h5 class="font-bold">Categories</h5>
+                    <ul class="list-group">
+                        @foreach ($categories as $category)
+                            <li class="list-group-item">
+                                <a href="{{ URL::to('post/'.$category->id) }}">
+                                    <span class="badge pull-right">15</span>
+                                    {!!html_entity_decode($category->name)!!}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <div class="tags m-b-lg l-h-2x">
+                        <a href class="label bg-primary">Bootstrap</a> <a href class="label bg-primary">Application</a>
+                        <a href class="label bg-primary">Apple</a> <a href class="label bg-primary">Less</a> <a
+                                href class="label bg-primary">Theme</a> <a href class="label bg-primary">Wordpress</a>
+                    </div>
+                    <h5 class="font-bold">Recent Posts</h5>
+                    <div>
+                        @foreach ($posts as $post)
+                            <div>
+                                <a class="pull-left thumb thumb-wrapper m-r">
+                                    <img src="{{ $post->avatar }}">
+                                </a>
+                                <div class="clear">
+                                    <a href="{{ URL::to('post_detail/'.$post->id) }}"
+                                       class="font-semibold text-ellipsis">{!!html_entity_decode($post->title)!!}</a>
+                                    <div class="text-xs block m-t-xs"><a href>English</a> {{ $post->updated_at }}</div>
+                                </div>
+                            </div>
+                            <div class="line"></div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- /content -->
+    @endif
+
 @endsection
--- resources/views/postdetail.blade.php
+++ resources/views/postdetail.blade.php
@@ -1,123 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
+    <!-- content -->
     <div class="container">
-        <div class="ui grid">
-            <div>
-                <div class="panel panel-default">
-                    <div>
-                        <?php
-                        foreach ($files as $file) : ?>
-                        <div class="form-group">
-                            <p>Danh sach cac file</p>
-                            <a href="{{ $file->url }}">{{ $file->name }}</a>
-                            @if (Auth::check())
-                            <button type="button" class="col-md-1 btn btn-primary" data-toggle="modal"
-                                    data-target="#updateFileModal">Update
-                            </button>
-                            <a class="col-md-1 btn btn-primary"
-                               href="{{ URL::to('delete_file/'.$file->id) }}">Delete</a>
-
-                            @endif
-                        </div>
-
-                        <!-- Modal File-->
-                        <div class="modal fade" id="updateFileModal" role="dialog">
-                            <div class="modal-dialog">
-
-                                <!-- Modal content-->
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">Update File</h4>
-                                    </div>
-                                    <form action="{{ url('updatefile/'.$file->id) }}" method="POST"
-                                          enctype="multipart/form-data">
-                                        {{ csrf_field() }}
-                                        <div class="form-group">
-                                            <label for="name" class="col-md-2 control-label">Name</label>
-                                            <div class="col-md-6">
-                                                <input id="name" type="text" class="form-control" name="name">
-                                            </div>
-                                            <div class="form-group">
-                                                <button class="btn btn-primary" type="submit">Update</button>
-                                            </div>
-                                        </div>
-                                    </form>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    <?php endforeach
-                    ?>
-
-                    <!-- File -->
-
-                        <!-- Trigger the modal with a button -->
-                        @if (Auth::check())
-                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#fileModal">
-                            Add new File
-                        </button>
-                        @endif
-
-                        <!-- Modal File-->
-                        <div class="modal fade" id="fileModal" role="dialog">
-                            <div class="modal-dialog">
-
-                                <!-- Modal content-->
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                        <h4 class="modal-title">UploadFile</h4>
-                                    </div>
-                                    <form action="{{ url('uploadfile/'.$post->id) }}" method="POST"
-                                          enctype="multipart/form-data">
-                                        {{ csrf_field() }}
-                                        <div class="form-group">
-                                            <label for="name" class="col-md-2 control-label">Title</label>
-                                            <div class="col-md-6">
-                                                <input id="name" type="text" class="form-control" name="name">
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="file" name="file"/>
-                                            <button class="btn btn-primary" type="submit">Upload</button>
-                                        </div>
-                                    </form>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
+        <div class="row">
+            <div class="col-sm-9">
+                <div class="blog-post">
                     <div class="panel">
-                        <div class="panel-heading">Post {{$post->title}}</div>
-                    </div>
-                    <div class="panel-body">
                         <div>
-                            Danh sách các bài viết trong cung chu de
+                            <img src="{{ $post->avatar }}" class="img-full">
                         </div>
-                        <?php
-                        foreach ($posts as $post_semilar) : ?>
-                        <div class="form-group">
-                            <label for="name" class="col-md-2 control-label">Name</label>
-                            <div class="col-md-6">
-                                <a href="{{ URL::to('post_detail/'.$post_semilar->id) }}">{!!html_entity_decode($post_semilar->title)!!}</a>
+                        <div class="wrapper-lg">
+                            <h2 class="m-t-none"><a
+                                        href="{{ URL::to('post_detail/'.$post->id) }}">{!!html_entity_decode($post->title)!!}</a>
+                            </h2>
+                            <div>
+                                {!!html_entity_decode($post->content)!!}
+                            </div>
+                            <div class="line line-lg b-b b-light"></div>
+                            <div class="text-muted">
+                                <i class="fa fa-user text-muted"></i> by <a href class="m-r-sm">Admin</a>
+                                <i class="fa fa-clock-o text-muted"></i> {{ $post->updated_at }}
+                                <a href class="m-l-sm"><i class="fa fa-comment-o text-muted"></i> 2 comments</a>
                             </div>
                         </div>
-                        <?php endforeach
-                        ?>
                     </div>
-                        <div>{!!html_entity_decode($post->content)!!}</div>
-                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <h5 class="font-bold">Recent Posts</h5>
+                <div>
+                    @foreach ($posts as $item)
+                        <div>
+                            <a class="pull-left thumb thumb-wrapper m-r">
+                                <img src="{{ $item->avatar }}">
+                            </a>
+                            <div class="clear">
+                                <a href="{{ URL::to('post_detail/'.$item->id) }}"
+                                   class="font-semibold text-ellipsis">{!!html_entity_decode($item->title)!!}</a>
+                                <div class="text-xs block m-t-xs"><a href>English</a> {{ $item->updated_at }}</div>
+                            </div>
+                        </div>
+                        <div class="line"></div>
+                    @endforeach
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
